c1bccc1c9b493869e4a1abc98f59a1f6d9f94513
Zhu Jia You <lxfdbzjy@163.com>
2019-05-10 15:47:17 +0800

    add model includes

---
+++ CNN/MODEL.cpp
@@ -0,0 +1,22 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                    Main routines for Model class                          */
+/*                                                                           */
+/*****************************************************************************/
+
+#define STRICT
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include <malloc.h>
+#include <new.h>
+#include <float.h>
+#include "CLASSES.h"
+
+
+
+
+
