1555c9efb5febabd5e02e800b0062080ab27f9b6
Zhu Jia You <lxfdbzjy@163.com>
2019-05-12 12:28:59 +0800

    Add SGD in pyANN

--- pyANN/network.py
+++ pyANN/network.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 
 class Network(object):
@@ -13,8 +14,45 @@ class Network(object):
             a = sigmoid(np.dot(a, w) + b)
         return a
 
-    def SGD(self, training_data, mini_batch_size, eta, test_data=None):
+    def SGD(self, training_data, epoches, mini_batch_size, eta, test_data=None):
+        '''
+        Train the neural network using mini-batch stochastic gradient descent.
+        The "training_data" is a list of tuples "(x, y)" representing the traning inputs and the 
+            desired outputs.
+        "eta" is learning rate.
+        If "test_data" is provided, the network will be evaluated with them after each opoch, and partial 
+            progress will be printed out.
+            This is usful for tracking progress, but sloes things down substantially.
+        '''
+        if test_data:
+            n_test = len(test_data)
+        n = len(training_data)
+        for j in xrange(epoches):
+            random.shuffle(training_data)
+            mini_batches = [training_data[k:k+mini_batch_size] for k in xrange(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+            if test_data:
+                print("Epoch {0}: {1} / {2}".format(j, self.evaluate(test_data), n_test))
+            else:
+                print("Epoch {0} complete".format(j))
 
+    def update_mini_batch(self, mini_batch, eta):
+        '''
+        Update the network's weights and biases by applying gradient descent using backpropagation to 
+            a single mini batch.
+        The "mini_batch" is a list of tuples "(x, y)".
+        '''
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+            self.weights = [w-(eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
+            self.biases = [b-(eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
+        
+            
 
 
 def sigmoid(z):
