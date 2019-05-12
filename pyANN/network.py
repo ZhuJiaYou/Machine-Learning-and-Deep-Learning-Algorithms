affb87fef22025c605cafdf8209fe085692fad77
Zhu Jia You <lxfdbzjy@163.com>
2019-05-12 09:27:26 +0800

    Add sigmoid, feedforward in pyANN

--- pyANN/network.py
+++ pyANN/network.py
@@ -7,3 +7,19 @@ class Network(object):
         self.sizes = sizes
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
+
+    def feedforward(self, a):
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(a, w) + b)
+        return a
+
+    def SGD(self, training_data, mini_batch_size, eta, test_data=None):
+
+
+
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
+
+if __name__ == '__main__':
+    print(sigmoid(0))
