15f3719b2fb47bd33f1aee1dfd13b869961c5969
Zhu Jia You <lxfdbzjy@163.com>
2019-05-17 15:56:05 +0800

    Add Backpropogation Function in pyANN

--- pyANN/network.py
+++ pyANN/network.py
@@ -22,7 +22,7 @@ class Network(object):
         "eta" is learning rate.
         If "test_data" is provided, the network will be evaluated with them after each opoch, and partial 
             progress will be printed out.
-            This is usful for tracking progress, but sloes things down substantially.
+            This is usful for tracking progress, but slows things down substantially.
         '''
         if test_data:
             n_test = len(test_data)
@@ -51,12 +51,54 @@ class Network(object):
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
             self.weights = [w-(eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
             self.biases = [b-(eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
-        
-            
+
+    def backprop(self, x, y):
+        '''
+        Return a tuple (nabla_b, nabla_w) representing the gradient for the cost function C_x.
+        nabla_b and nabla_w are layer-by-layer lists of numpy arrays, 
+        similar to self.biases and self.weights.
+        '''
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        # feedforward
+        activation = x
+        activations = [x]  # list to store all the activations, layer by layer
+        zs = []  # list to store all the z vectors, layer by layer
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        # backward pass
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        # Note that the variable l in the loop uses a reverse order.
+        # Here, l=1 means the last layer of neurons, l=2 is the second-last layer, and so on.
+        for l in xrange(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w = np.dot(delta, activations[-l - 1].transpose())
+        return (nabla_b, nabla_w)
+
+    def cost_derivative(self, output_activations, y):
+        '''
+        Return the vector of partial derivatives partial C_x / partial a for the output activations
+        '''
+        return (output_activations - y)           
 
 
 def sigmoid(z):
     return 1.0 / (1.0 + np.exp(-z))
+
+
+def sigmoid_prime(z):
+    '''
+    Derivative of the sigmoid function.
+    '''
+    return sigmoid(z) * (1 - sigmoid(z))
 
 
 if __name__ == '__main__':
