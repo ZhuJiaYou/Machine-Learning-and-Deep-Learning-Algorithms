b1be0cacd80f4d7c945e4292df42b4a9e8fda79d
Zhu Jia You <lxfdbzjy@163.com>
2019-05-21 09:29:20 +0800

    Add network2.py in pyANN

---
+++ pyANN/network2.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+
+class Network():
+    def __init__(self, sizes, cost=CrossEntropyCost):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.default_weight_initializer()
+        self.cost = cost
+
+    def default_weight_initializer(self):
+        self.biases = [np.random.randn(y, 1) for y in self.sizes[1:]]
+        self.weights = [np.random.randn(y, x) / np.sqrt(x) for x, y in zip(sizes[:-1], self.sizes[1:])]
+
+    def large_weight_initializer(self):
+        self.biases = [np.random.randn(y, 1) for y in self.sizes[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], self.sizes[1:])]
+
+
+class CrossEntropyCost():
+    @staticmethod
+    def fn(a, y):
+        return np.sum(np.nan_to_num(-y*np.log(a) - (1-y)*np.log(1-a)))
+
+    @staticmethod
+    def delta(z, a, y):
+        return (a - y)
+
+
+class QuadraticCost():
+    @staticmethod
+    def fn(a, y):
+        return 0.5 * np.linalg.norm(a - y) ** 2
+
+    @staticmethod
+    def delta(z, a, y):
+        return (a - y) * sigmoid_prime(z)
+
+
+def sigmoid_prime(z):
+    '''
+    Derivative of the sigmoid function.
+    '''
+    return sigmoid(z) * (1 - sigmoid(z))
