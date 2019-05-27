128a57b63b08830ee1b0c5255865ec242f8b9876
Zhu Jia You <lxfdbzjy@163.com>
2019-05-27 08:38:22 +0800

    Add overfitting.py

--- pyANN/overfitting.py
+++ pyANN/overfitting.py
@@ -71,3 +71,75 @@ def make_plots(filename, num_epoches,
     plot_overlay(test_accuracy, training_accuracy, num_epoches, 
                  min(test_accuracy_xmin, training_accuracy_xmin), training_set_size)
 
+
+def plot_training_cost(training_cost, num_epoches, training_cost_xmin):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(training_cost_xmin, num_epoches), training_cost[training_cost_xmin:num_epoches], 
+                      color='#2A6EA6')
+    ax.set_xlim([training_cost_xmin, num_epoches])
+    ax.grid(True)
+    ax.set_xlabel('Epoch')
+    ax.set_title('Cost on the training data')
+    plt.show()
+
+
+def plot_test_cost(test_cost, num_epoches, test_cost_xmin):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(test_cost_xmin, num_epoches), test_cost[test_cost_xmin:num_epoches], 
+                      color='#2A6EA6')
+    ax.set_xlim([test_cost_xmin, num_epoches])
+    ax.grid(True)
+    ax.set_xlabel('Epoch')
+    ax.set_title('Cost on the test data')
+    plt.show()
+
+
+def plot_test_accuracy(test_accuracy, num_epoches, test_accuracy):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(test_accuracy_xmin, num_epoches), 
+            [accuracy / 100.0 for accuracy in test_accuracy[test_accuracy_xmin:num_epoches]],
+            color='#2A6EA6')
+    ax.set_xlim([test_accuracy_xmin, num_epoches])
+    ax.grid(True)
+    ax.set_xlabel('Epoch')
+    ax.set_title('Accuracy(%) on the test data')
+    plt.show()
+
+
+def plot_training_accuracy(training_accuracy, num_epoches, training_accuracy_xmin, training_set_size):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(training_accuracy_xmin, num_epoches), 
+            [accuracy * 100.0 / training_set_size 
+                for accuracy in training_accuracy[training_accuracy_xmin:num_epoches]],
+            color='#2A6EA6')
+    ax.set_xlim([training_accuracy_xmin, num_epoches])
+    ax.grid(True)
+    ax.set_xlabel('Epoch')
+    ax.set_title('Accuracy(%) on the training data')
+    plt.show()
+
+
+def plot_overlay(test_accuracy, training_accuracy, num_epoches, xmin, training_set_size):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(xmin, num_epoches), 
+            [accuracy / 100.0 for accuracy in test_accuracy],
+            color='#2A6EA6', 
+            label='Accuracy on the test data')
+    ax.plot(np.arange(xmin, num_epoches), 
+            [accuracy * 100.0 / training_set_size for accuracy in training_accuracy],
+            color='#FFA933', 
+            label='Accuracy on the training data'))
+    ax.set_xlim([test_accuracy_xmin, num_epoches])
+    ax.grid(True)
+    ax.set_xlabel('Epoch')
+    ax.set_title('Accuracy(%) on the test data')
+    plt.show()
+
+
+if __name__ == '__main__':
+
