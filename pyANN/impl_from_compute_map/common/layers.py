0d67094f76943e0413437f56b64cb1872e9e3685
Zhu Jia You <lxfdbzjy@163.com>
2019-06-12 10:37:30 +0800

    Add Pooling layer in pyANN/impl_from_compute_map/common/layers.py

--- pyANN/impl_from_compute_map/common/layers.py
+++ pyANN/impl_from_compute_map/common/layers.py
@@ -1,6 +1,46 @@
 import numpy as np
-from common.funcs import *
-from common.util import im2col
+from funcs import *
+from util import im2col, col2im
+
+
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+
+        self.x = None
+        self.arg_max = None
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = (H + 2*self.pad - FH) // self.stride + 1
+        out_w = (W + 2*self.pad - FW) // self.stride + 1
+
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h*self.pool_w)
+
+        arg_max = np.argmax(col, axis=1)
+        out = np.max(col, axis=1)
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.arg_max = argmax
+    
+        return out
+
+    def backward(self, dout):
+        dout = dout.transpose(0, 2, 3, 1)
+
+        pool_size = self.pool_h * self.pool_w
+        dmax = np.zeros((dout.size, pool_size))
+        dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
+        dmax = dmax.reshape(dout.shape + (pool_size, ))
+
+        dcol = dmax.reshape(dmax.shape[0]*dmax.shape[1]*dmax.shape[2], -1)
+        dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
+        return dx
 
 
 class Convolution:
@@ -21,8 +61,8 @@ class Convolution:
     def forward(self, x):
         FN, C, FH, FW = self.w.shape
         N, C, H, W = x.shape
-        out_h = (H + 2*pad - filter_h) // stride + 1
-        out_w = (W + 2*pad - filter_w) // stride + 1
+        out_h = (H + 2*self.pad - FH) // self.stride + 1
+        out_w = (W + 2*self.pad - FW) // self.stride + 1
 
         col = im2col(x, FH, FW, self.stride, self.pad)
         col_w = self.w.reshape(FN, -1).T
@@ -48,11 +88,6 @@ class Convolution:
         dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
         return dx
         
-
-
-
-        
-
 
 class Dropout:
     def __init__(self, dropout_ratio=0.5):
